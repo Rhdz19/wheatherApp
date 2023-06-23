dab2fdd8951bea545562c5ff7fb18aafa9fe978e
DanaheMurillo <danahe.murillo.ruiz@gmail.com>
2023-06-23 12:28:55 -0500

    Creacion de componentes para para la vista y el servicio del clima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
+import { WeatherComponent } from './components/weather/weather.component';
+import { WeatherService } from './services/weather.service';
+
+@NgModule({
+  declarations: [AppComponent, WeatherComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule],
+  providers: [WeatherService],
+  bootstrap: [AppComponent],
+})
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -11,3 +22,4 @@ import { IonicModule } from '@ionic/angular';
 export class AppComponent {
   constructor() {}
 }
+export class AppModule {}
