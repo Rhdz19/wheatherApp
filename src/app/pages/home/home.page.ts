717ab12700301572eb7f23632218502208065200
Rhdz19 <L19530321@cancun.tecnm.mx>
2023-06-30 09:51:46 -0500

    Fix

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,64 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from 'src/app/services/weather.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Storage } from '@ionic/storage';
 
-
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
-
-import { SharedModule } from 'src/app/shared/shared.module';
-import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-home',
-  templateUrl: '../home/home.page.html',
-  styleUrls: ['../home/home.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, SharedModule]
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
 })
-export class HomePage {
-  public weather: any;
-  public city: string = '';
-  public weatherForm: FormGroup = new FormGroup({
-    city: new FormControl('', Validators.required)
-  });
-  constructor(
-    private weatherService: WeatherService,
-    private storage: Storage
-  ) {
-    this.weatherForm = new FormGroup({
-      city: new FormControl('', Validators.required)
-    });
-  }
+export class HomePage implements OnInit {
+  city: string = ''; // Variable para almacenar la ciudad ingresada
+  weather: any;
 
-  search() {
-    this.storage.set('city', this.city);
+  constructor(private weather_service: WeatherService) {}
 
-    this.weatherService.getWeatherFromApi(this.city).subscribe((weather) => {
-      this.weather = weather;
-      console.log(weather);
-    });
-  }
-
-  ngOnInit() {
-    this.getWeather();
-  }
+  ngOnInit() {}
 
   getWeather() {
-    this.storage.get('city').then((city) => {
-      if (city === null) {
-        this.weatherService.getWeatherFromApi('cancun').subscribe((weather) => {
-          this.weather = weather;
-          console.log('clima de cancun', weather);
-        });
-      } else {
-        this.weatherService.getWeatherFromApi(city).subscribe((weather) => {
-          this.weather = weather;
-          console.log(weather);
-        });
-      }
+    this.weather_service.getWeather(this.city).subscribe((data) => {
+      this.weather = data;
     });
   }
 }
